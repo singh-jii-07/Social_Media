900c9a625e4a4b331020a3bd289c69a6e538ff17
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-12-08 22:33:08 -0500

    message models done

---
+++ Backend/App/Models/MessageModels.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    message: {
+      type: String,
+      default: "",
+    },
+
+    messageType: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageSchema);
