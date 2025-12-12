c478b0049f1487401db0d72380fab120db305cb1
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-12-11 22:53:06 -0500

    user routes done

---
+++ Backend/App/Routes/UserRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import isAuthenticated from "../Middleware/isAuthenticated.js";
+import {
+  register,
+  login,
+  logout,
+  getProfile,
+  editProfile,
+  getSuggestedUsers,
+  followOrUnfollow,
+} from "../Controllers/UserController.js";
+
+import upload from "../Middleware/Multer.js";
+const userRoutes = express.Router();
+
+userRoutes.post("/register", register);
+userRoutes.post("/login", login);
+userRoutes.post("/logout", logout);
+userRoutes.get("/profile/:id", isAuthenticated, getProfile);
+userRoutes.post(
+  "/profile/edit",
+  isAuthenticated,
+  upload.single("profilePhoto"),
+  editProfile
+);
+userRoutes.get("/suggested", isAuthenticated, getSuggestedUsers);
+userRoutes.post("/followorunfollow/:id", isAuthenticated, followOrUnfollow);
+export default router;
