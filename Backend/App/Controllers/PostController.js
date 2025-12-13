479889abe1cb32915510a3f9a00ea8e58573f5ad
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-12-12 22:15:57 -0500

    new post controller done

---
+++ Backend/App/Controllers/PostController.js
@@ -0,0 +1,47 @@
+import sharp from "sharp";
+import cloudinary from "../Util/cloudinary.js";
+import Post from '../Models/PostModels.js'
+import User from '../Models/UserModels.js'
+
+const addNewPost = async (req, res) => {
+    try {
+        const { caption } = req.body;
+        const image = req.file;
+        const authorId = req.id;
+
+        if (!image) return res.status(400).json({ message: 'Image required' });
+
+        // image upload 
+        const optimizedImageBuffer = await sharp(image.buffer)
+            .resize({ width: 800, height: 800, fit: 'inside' })
+            .toFormat('jpeg', { quality: 80 })
+            .toBuffer();
+
+        // buffer to data uri
+        const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString('base64')}`;
+        const cloudResponse = await cloudinary.uploader.upload(fileUri);
+        const post = await Post.create({
+            caption,
+            image: cloudResponse.secure_url,
+            author: authorId
+        });
+        const user = await User.findById(authorId);
+        if (user) {
+            user.posts.push(post._id);
+            await user.save();
+        }
+
+        await post.populate({ path: 'author', select: '-password' });
+
+        return res.status(201).json({
+            message: 'New post added',
+            post,
+            success: true,
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {addNewPost}
