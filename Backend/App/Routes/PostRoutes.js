736ed3ebda2a858b26464ebf42c6a3483cc4552c
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-12-13 00:36:52 -0500

    Post route Done

---
+++ Backend/App/Routes/PostRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import isAuthenticated from "../Middleware/isAuthenticated .js";
+import upload from "../Middleware/Multer.js"; 
+import {
+  addNewPost,
+  getAllPost,
+  getUserPost,
+  likePost,
+  dislikePost,
+  addComment,
+  getCommentsOfPost,
+  deletePost,
+  bookmarkPost,
+} from "../Controllers/PostController.js";
+
+const PostRouter = express.Router();
+
+PostRouter.post("/addpost",isAuthenticated, upload.single('image'), addNewPost);
+PostRouter.get ("/getpost",isAuthenticated,getAllPost);
+PostRouter.get ("/getpostuser/:id",isAuthenticated, getUserPost);
+PostRouter.get ("/like/:id",isAuthenticated, likePost);
+PostRouter.get ("/dislike/:id",isAuthenticated, dislikePost);
+PostRouter.post ("/comment/:id",isAuthenticated, addComment);
+PostRouter.post ("/comment/all/:id",isAuthenticated, getCommentsOfPost);
+PostRouter.delete ("/delete/:id",isAuthenticated, deletePost);
+PostRouter.get ("/bookmark/:id",isAuthenticated, bookmarkPost);
+
+export default PostRouter;
--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import userRoutes from './App/Routes/UserRoutes.js'
+import Postrouter from './App/Routes/PostRoutes.js'
+
 
 const app = express();
 dotenv.config();
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/post", Postrouter);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_DB; 
